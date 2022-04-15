adewes/have-i-been-bloomed

commit 2162e73166323935d99472a8ac1cbfd712eb4523

Fixed gzip option, Makefile. Improved README.

Makefile
@@ -1,19 +1,33 @@
 #!/bin/bash
 
+N := 501636842
+
 # download pwned passwords file
 pwned-passwords-2.0.txt.7z:
 	wget https://downloads.pwnedpasswords.com/passwords/pwned-passwords-2.0.txt.7z
 
+test-filter:
+	bloom --gzip create -p 1e-6 -n 100 pwned-passwords-2.0.bloom.test.gz
+	7z x pwned-passwords-2.0.txt.7z -so | awk -F":" '{print $$1}' | head -n 100 | bloom --gzip insert pwned-passwords-2.0.bloom.test.gz
+
 # create the Bloom filter
 pwned-passwords-2.0.bloom: pwned-passwords-2.0.txt.7z
-	bloom --gzip create -p 1e-6 -n 501636842 pwned-passwords-2.0.bloom
-	7z x pwned-passwords-2.0.txt.7z -so | awk -F":" '{print $1}' | bloom --gzip insert pwned-passwords-2.0.bloom
+	bloom --gzip create -p 1e-6 -n ${N} pwned-passwords-2.0.bloom.gz
+	7z x pwned-passwords-2.0.txt.7z -so | awk -F":" '{print $$1}' | bloom --gzip insert pwned-passwords-2.0.bloom.gz
 
-bloom-filter: pwned-passwords-2.0.bloom
+bloom-filter: pwned-passwords-2.0.bloom.gz
 
 bloom-tool:
 	go get github.com/dcso/bloom
 	go install github.com/dcso/bloom/bloom
+
+test: bloom-tool server test-filter
+
+run:
+	hibb
+
+run-test:
+	hibb -f pwned-passwords-2.0.bloom.test.gz
 
 server:
 	go get ./...

.gitignore
@@ -1,4 +1,4 @@
-*.bloom
+*.gz
 *.7z
 
 

cmd/hibb/server.go
@@ -44,7 +44,7 @@ func main() {
 	bind := flag.String("b", "0.0.0.0:8000", "The address to which to bind")
 	flag.Parse()
 	fmt.Printf("Loading Bloom filter from %s...\n", *filename)
-	filter, err = bloom.LoadFilter(*filename, false)
+	filter, err = bloom.LoadFilter(*filename, true)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(-1)

commit dc57fd771e9da3f638948d46149df62a530b6869

Fix "No rule to make target / file not found" error
This is the error message that might be caused by 2162e73166323935d9947:

1. make: *** No rule to make target 'pwned-passwords-2.0.bloom.gz', needed by 'bloom-filter'.  Stop.
2. Error: open /home/user/have-i-been-bloomed/pwned-passwords-2.0.bloom.gz: no such file or directory

Makefile
@@ -11,7 +11,7 @@ test-filter:
 	7z x pwned-passwords-2.0.txt.7z -so | awk -F":" '{print $$1}' | head -n 100 | bloom --gzip insert pwned-passwords-2.0.bloom.test.gz
 
 # create the Bloom filter
-pwned-passwords-2.0.bloom: pwned-passwords-2.0.txt.7z
+pwned-passwords-2.0.bloom.gz: pwned-passwords-2.0.txt.7z
 	bloom --gzip create -p 1e-6 -n ${N} pwned-passwords-2.0.bloom.gz
 	7z x pwned-passwords-2.0.txt.7z -so | awk -F":" '{print $$1}' | bloom --gzip insert pwned-passwords-2.0.bloom.gz
 